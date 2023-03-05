7d40584ef78a68061b8e3de8ced345b9142fb675
JEGORS GVOZDJEVS 221RMC148 <125265638+Jegors-Gvozdjevs@users.noreply.github.com>
2023-03-06 01:56:05 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -38,22 +38,11 @@ def main():
         file = input()
         if "a" not in file:
             with open("test/" + file, 'r')as f:
-                    n = int(file.readline())
-                    parents = list(map(int, input().split()))
-                    ans = compute_height(n, parents)
-                    print(ans)
-                    
+                n = int(f.readline())
+                parents = list(map(int, input().split()))
+                ans = compute_height(n, parents)
+                print(ans)
 
-     
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    #pass
-
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
