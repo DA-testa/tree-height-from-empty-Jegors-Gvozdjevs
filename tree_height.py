9b3af581fcf27eddd5ea7e11533a4f77f7ac4a18
JEGORS GVOZDJEVS 221RMC148 <125265638+Jegors-Gvozdjevs@users.noreply.github.com>
2023-03-06 01:25:47 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -25,28 +25,42 @@ def compute_height(n, parents):
 
 
 def main():
+    # implement input form keyboard and from files
     fileorno = input("i or f")
     ok = True
     while ok:
-
         if fileorno == "I":
             n = int(input("nodes"))
             parents = list(map(int, input().split()))
             ans = compute_height(n, parents)
             print(ans)
             ok = False
-
-        if fileorno == "F":
+            
+                # let user inputfile name to use, don't allow file names with letter a
+                # account for github input inprecision
+        if "I" in fileorno or "i" in fileorno:
             file =input()
-            with open("test/" + file, 'r')as f:
-                    n = int(file.readline())
-                    parents = list(map(int, input().split()))
-                    ans = compute_height(n, parents)
-                    print(ans)
-                    ok = False
+            if "a" not in file:
+                with open("test/" + file, 'r')as f:
+                        n = int(file.readline())
+                        parents = list(map(int, input().split()))
+                        ans = compute_height(n, parents)
+                        print(ans)
+                        ok = False
                     
 
+     
+    
+    # input number of elements
+    # input values in one variable, separate with space, split these values in an array
+    # call the function and output it's result
+    #pass
+
+# In Python, the default limit on recursion depth is rather low,
+# so raise it here for this problem. Note that to take advantage
+# of bigger stack, we have to launch the computation in a new thread.
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
 main()
+#print(numpy.array([1,2,3]))
